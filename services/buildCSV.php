1fde85d1a308541fffe676f9a563652af716a91e
plandreau <plandreau@addictic.fr>
2019-07-31 16:58:51 +0200

    oupsi

--- services/buildCSV.php
+++ services/buildCSV.php
@@ -4,8 +4,6 @@ if(!$datas) return false;
 $filename = array_key_exists('filename', $_POST) ? $_POST['filename'] : 'tabler-export.csv';
 $file = "tmp/$filename";
 
-// $datas = str_replace( $_POST['data'] );
-
 file_put_contents('../'.$file, $datas);
 
 echo json_encode($file);
